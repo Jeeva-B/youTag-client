56cfebffcd6ebf7ce177209da94a165a67e997b7
jeeva <jeevasubash64@gmail.com>
2022-06-21 23:15:31 +0530

    Input Box.

--- src/component/Tags.js
+++ src/component/Tags.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Container } from '@mui/system';
+import { Grid } from '@mui/material';
+import InputField from '../assets/common/InputField';
 
 const Tags = () => {
     return (
-        <div>Tags</div>
+        <Container>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <InputField />
+            </Grid>
+        </Container>
     );
 };
 
